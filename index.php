74c5999efc327b8a866c6d604b4f17ec4bbf85a6
AxtrlL <azzmmm1@gmail.com>
2022-09-13 17:12:02 +0700

    tambah index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HI Welcome</title>
+</head>
+<body>
+    <h1><center>Bjoo orang Bjo Ngga masuk angin
+
+    </center>
+
+</h1>
+    
+    
+
+
+    
+</body>
+</html>
